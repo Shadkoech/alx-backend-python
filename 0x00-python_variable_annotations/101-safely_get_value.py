ce96c03a4c0bacdad662c32005679473c747eed0
Shadkoech <shadrackipruto@gmail.com>
2024-03-14 14:11:50 +0300

    Adding type annotations to the function

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+A module that adds type annotation to a given function
+def safely_get_value(dct, key, default = None):
+    if key in dct:
+        return dct[key]
+    else:
+        return default
+"""
+
+from typing import TypeVar, Mapping, Any, Union
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any, default:
+                     Union[T, None] = None) -> Union[Any, T]:
+    """Add type annotations to the function"""
+
+    if key in dct:
+        return dct[key]
+    else:
+        return default
