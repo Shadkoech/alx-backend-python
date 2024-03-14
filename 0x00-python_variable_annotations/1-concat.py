ffb155d56cd66d8740678690b8246f7a54b42517
Shadkoech <shadrackipruto@gmail.com>
2024-03-14 11:25:31 +0300

    Type-annotated funtion that concatenates two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Python module that showcases basic variable annotation
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Type-annotated function that concatenates two
+    strings """
+
+    return (str1 + str2)
