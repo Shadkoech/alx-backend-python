c51448aedf69e2f25e402767d8516e135727e49f
Shadkoech <shadrackipruto@gmail.com>
2024-03-14 11:45:00 +0300

    Type-annotated funtion that returns a string representation of a function

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Python module that showcases basic type annotation """
+
+
+def to_str(n: float) -> str:
+    """
+    Type-annotated function that takes a float and returns
+    string representation of that float """
+
+    return str(n)
