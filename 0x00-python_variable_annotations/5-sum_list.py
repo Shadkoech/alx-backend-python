2ada80bb781696f16108302891b988811d2cabd3
Shadkoech <shadrackipruto@gmail.com>
2024-03-14 12:01:33 +0300

    Type-annotated funtion that returns sum of a list as a float

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Python module that showcases basic type annotation """
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Type-annotated function that returns the sum of
+    floats in the given list """
+
+    return sum(input_list)
